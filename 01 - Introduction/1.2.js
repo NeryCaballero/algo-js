05b39065ad1a39a982c79d8178419faae9195d5e
NeryCaballero <nerycaballero24@gmail.com>
2020-10-16 17:06:02 +0200

    Updated directory name

---
+++ 01 - Introduction/1.2.js
@@ -0,0 +1,9 @@
+// Exercise 1.2
+// Define three variables: name, firstName and city. 
+// Display them like this: "Your name is Gerard Lambert and you live in Paris.".
+
+let name = "Caballero";
+let firstName = "Nery";
+let city = "Gembloux";
+
+console.log ("My name is " + firstName + " " + name + " and I live in " + city + ".");
